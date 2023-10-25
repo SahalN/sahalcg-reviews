2d5f4b07e1bdeeadb334893c5822634adda966dd
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-25 13:30:06 +0700

    Adding dangerouslysetInnerHtml with hederIds and mangle

--- app/reviews/the-race/page.jsx
+++ app/reviews/the-race/page.jsx
@@ -5,11 +5,12 @@ import { marked } from "marked";
 import { readFile } from "node:fs/promises";
 
 export default async function TheracePage() {
-  const html = await readFile("./content/reviews/the-race.md", "utf-8");
-  const text = marked(html);
+  const text = await readFile("./content/reviews/the-race.md", "utf-8");
+  const html = marked(text, { hederIds: false, mangle: false });
   return (
     <>
-      <Heading>The Race CGI Review</Heading> {text}
+      <Heading>The Race CGI Review</Heading>
+      <article dangerouslySetInnerHTML={{ __html: html }} />
     </>
   );
 }
