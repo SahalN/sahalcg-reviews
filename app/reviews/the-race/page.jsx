c7e32c138b1e7ae1848c19fac2504c9b6e42d770
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-25 13:20:38 +0700

    add marked and content reviews

--- app/reviews/the-race/page.jsx
+++ app/reviews/the-race/page.jsx
@@ -1,13 +1,15 @@
 /** @format */
 
 import Heading from "@/components/Heading";
+import { marked } from "marked";
 import { readFile } from "node:fs/promises";
 
 export default async function TheracePage() {
   const html = await readFile("./content/reviews/the-race.md", "utf-8");
+  const text = marked(html);
   return (
     <>
-      <Heading>The Race CGI Review</Heading> {html}
+      <Heading>The Race CGI Review</Heading> {text}
     </>
   );
 }
